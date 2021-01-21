9a04f461d98a7bc5b4babea9316200dc56ad48a8
Vivi Nguyen <vivipnguyen@gmail.com>
2021-01-21 15:14:44 -0800

    complete demo

--- admin.py
+++ admin.py
@@ -10,9 +10,13 @@ app.config['SECRET_KEY'] = 'secret'
 
 db = SQLAlchemy(app)
 
+admin = Admin(app)
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
+
+admin.add_view(ModelView(Person, db.session))
 
 if __name__ == '__main__':
     app.run(debug=True)
